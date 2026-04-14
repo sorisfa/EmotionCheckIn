337af385bd21ffa79de59e00dcc86918e4031686
sorisfa <101617672+sorisfa@users.noreply.github.com>
2026-04-14 14:21:15 +0900

    Create src/App.tsx file with main application component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Emotion Check-In</h1>
+            <p>Your main application component is ready!</p>
+        </div>
+    );
+};
+
+export default App;
